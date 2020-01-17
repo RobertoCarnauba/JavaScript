85035d55ec721a00464f1025c8620078400453df
Roberto Carnaúba Dias <betocarnauba@hotmail.com>
2020-01-16 22:35:10 -0300

    update
    news

--- 05 - ES6/main.js
+++ 05 - ES6/main.js
@@ -1,19 +1,36 @@
-class TodoList {
+class List {
     constructor(){
-        this.todos = [];
+        var data = []
+        //localStorage.setItem(data);
+        saveToStorage(data)
     }
 
     addTodo(){
-        this.todos.push('Novo Todo');
-        console.log(this.todos)
+        
+        let todoinput = document.querySelector("#input_todo").value
+        this.data.push(todoinput)
+        //this.data.push(TodoList);
+        console.log(this.data)
     }
 
 }
 
+class TodoList extends List {
+    constructor(){
+        super()
+        
+    }
+   
+}
 
 
 const MinhaLista = new TodoList();
 
 document.getElementById("novotodo").onclick = function(){
-    MinhaLista.addTodo()
+    MinhaLista.addTodo('Novo Todo')
+    
+}
+
+function saveToStorage(){
+    localStorage.setItem('list', JSON.stringify(data))
 }
