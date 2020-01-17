1e2a1483d1c3f0376c51c20f283c7624a2dd144c
Roberto Carnaúba Dias <betocarnauba@hotmail.com>
2020-01-16 21:11:21 -0300

    update

---
+++ 05 - ES6/main.js
@@ -0,0 +1,19 @@
+class TodoList {
+    constructor(){
+        this.todos = [];
+    }
+
+    addTodo(){
+        this.todos.push('Novo Todo');
+        console.log(this.todos)
+    }
+
+}
+
+
+
+const MinhaLista = new TodoList();
+
+document.getElementById("novotodo").onclick = function(){
+    MinhaLista.addTodo()
+}
